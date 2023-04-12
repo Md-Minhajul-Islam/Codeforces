79ec2979f48b965b50eac9dc64d6c08ead8c6cd7
Minhaj <minhajulislammuhammad@gmail.com>
2023-04-12 14:26:30 +0600

    G_Katryoshka.c

--- G Set/G_Katryoshka.c
+++ G Set/G_Katryoshka.c
@@ -5,24 +5,20 @@ int main()
     long long int e, m, b, total = 0;
     scanf("%lld %lld %lld", &e, &m, &b);
 
-    if(m > 0 && b > 0 && e > 0)
-    {
-        if(m < b && m < e)
-            total = m;
-        else if(b < e)
-            total = b;
-        else 
-            total = e;
-        m -= total; b -= total; e -= total;
-    }
-    if(e > 1 && b > 0)
-    {
-        e /= 2;
-        if(e < b)
-            total += e;
-        else  
-            total += b;
-    }
+    if(m < b && m < e)
+        total = m;
+    else if(b < e)
+        total = b;
+    else 
+        total = e;
+    m -= total; b -= total; e -= total;
+
+    e /= 2;
+    if(e < b)
+        total += e;
+    else  
+        total += b;
+
     printf("%lld\n", total);
 
     return 0;
