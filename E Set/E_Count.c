0c2218dd94f2920c0dc5067f443406e1e790b4f4
Minhaj <minhajulislammuhammad@gmail.com>
2023-04-17 18:58:42 +0600

    E_Count.c

---
+++ E Set/E_Count.c
@@ -0,0 +1,13 @@
+// Q: https://codeforces.com/group/MWSDmqGsZm/contest/219856/problem/E
+#include<stdio.h>
+int main()
+{
+    char s[1000001];
+    scanf("%s", s);
+    int sum = 0;
+    for(int i = 0; s[i] != '\0'; i++)
+        sum += s[i]- '0';
+    printf("%d", sum);
+    return 0;
+}
+
