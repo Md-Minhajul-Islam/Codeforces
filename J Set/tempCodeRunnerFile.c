691a196db326cedc11c2afbfac375dc240ae2178
Minhaj <minhajulislammuhammad@gmail.com>
2023-04-20 14:48:20 +0600

    Added

---
+++ J Set/tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+int len = strlen(s);
+    // int count[26] = {0};
+    // for(int i = 0; i < len; i++)
+    //     count[s[i]-'a']++;
+    // for(int i = 0; i < len; i++){ 
+    //     if(count[i] > 0)
+    //         printf("%c : %d\n", i+'a', count[i]);
+    // }
