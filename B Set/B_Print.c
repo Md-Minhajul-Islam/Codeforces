de8ac92d70b2b72fdf9e8de077c880c717598473
Minhaj <minhajulislammuhammad@gmail.com>
2023-05-03 23:08:44 +0600

    added B_Print.c

---
+++ B Set/B_Print.c
@@ -0,0 +1,15 @@
+// Q: https://codeforces.com/group/MWSDmqGsZm/contest/223205/problem/B
+#include<stdio.h>
+void print(int n)
+{
+    for(int i = 1; i < n; i++)
+        printf("%d ", i);
+    printf("%d", n);
+}
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    print(n);
+    return 0;
+}
