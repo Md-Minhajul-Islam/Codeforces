91f660d924a0d1029f62b51c9b20d1cbd8bbee02
Minhaj <minhajulislammuhammad@gmail.com>
2023-05-23 23:03:49 +0600

    added D. Difference.cpp

---
+++ D Set/Difference.cpp
@@ -0,0 +1,10 @@
+// Q: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/D
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    cout << "Difference = " << a*b - c*d;
+    return 0;
+}
