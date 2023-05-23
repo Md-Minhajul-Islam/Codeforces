2e11b4d7e60ca22e66c33dc0110eef3be54642ff
Minhaj <minhajulislammuhammad@gmail.com>
2023-05-23 23:09:28 +0600

    added E. Area_of_a_Circle.cpp

---
+++ E Set/Area_of_a_Circle.cpp
@@ -0,0 +1,10 @@
+// Q: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/E
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    double r;
+    cin >> r;
+    cout << fixed << setprecision(9) << 3.141592653*r*r;
+    return 0;
+}
