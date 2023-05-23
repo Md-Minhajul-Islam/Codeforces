7c00291330dff84c25982ef5694c292f584ea14b
Minhaj <minhajulislammuhammad@gmail.com>
2023-05-23 23:15:36 +0600

    added F. Digit_Summation.cpp

---
+++ F Set/Digits_Summation.cpp
@@ -0,0 +1,10 @@
+// Q: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/F
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n, m;
+    cin >> n >> m;
+    cout << n%10 + m%10;
+    return 0;
+}
