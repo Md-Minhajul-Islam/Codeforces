f7269b20264b0e1bd45418a47226b95e5ecb46cb
Minhaj <minhajulislammuhammad@gmail.com>
2023-05-24 09:59:09 +0600

    added G. Summation_form_1_to_N.cpp

---
+++ G Set/Summation_from_1_to_N.cpp
@@ -0,0 +1,10 @@
+// Q: https://codeforces.com/group/MWSDmqGsZm/contest/219158/problem/G
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int n;
+    cin >> n;
+    cout << n*(n+1)/2;
+    return 0;
+}
